f7b54559468c661a52bc3d2a81ce76ca0f61eaed
Kongbi-Zion <kongbizion3@gmail.com>
2024-10-31 14:40:43 +0100

    first comment

---
+++ components/Comment.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="w-full text-sm">
+    <div class="w-full">
+      <div class="flex items-center gap-3">
+        <img src="/profile.webp" class="h-8 w-8 rounded-full" />
+        <div class="flex">
+          <p class="font-medium text-gray-100">Ro</p>
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M12 14a2 2 0 1 1 0-4a2 2 0 0 1 0 4Z"
+              />
+            </svg>
+          </span>
+          <span class=""> <span>4h</span></span>
+        </div>
+      </div>
+    </div>
+
+    <div class="w-full px-11 space-y-5">
+      <p>
+        In an era defined by rapid technological advancements and constant
+        shifts in the global landscape, adaptability has emerged as a vital
+        skill. Whether in personal life or professional settings, being able to
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// defineProps({
+//   number_of_comments: {
+//     type: Number,
+//     required: true,
+//   },
+// });
+</script>
